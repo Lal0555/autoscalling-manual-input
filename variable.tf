2b18d4deb078e2e885572bcf007e907cf3b5d4e4
mintuky1508 <mintuky1508@gmail.com>
2024-12-01 21:20:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,21 @@
+# variables.tf
+
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "The AMI ID to use for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "The instance type for the EC2 instance"
   type        = string
 }
 
 variable "security_group_id" {
-  description = "Security Group ID"
+  description = "The ID of the security group"
   type        = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID"
+  description = "The ID of the subnet"
   type        = string
 }
