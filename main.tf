416a584bd2e36adf26149e8101495b21809eff2c
mintuky1508 <mintuky1508@gmail.com>
2024-12-01 21:10:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,6 @@
 provider "aws" {
   region = "us-east-1"  # You can change this if needed
 }
-
-# Prompt for input values
-variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The instance type for the EC2 instance"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet"
-  type        = string
-}
-
 # Launch Template
 resource "aws_launch_template" "example" {
   name          = "example-launch-template"
